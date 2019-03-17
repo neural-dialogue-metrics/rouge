1e3f7760b714ba794580ee9d1343be4cab779362
cgsdfc <cgsdfc@126.com>
2019-03-17 16:56:50 +0800

    Test pass for rouge-1.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,9 +2,31 @@ from pythonrouge import Pythonrouge
 
 
 def make_rouge(**kwargs):
+    """
+    Create a Pythonrouge according to our settings.
+    :param kwargs:
+    :return:
+    """
     kwargs.setdefault('summary_file_exist', False)
     kwargs.setdefault('resampling', False)
     kwargs.setdefault('stopwords', False)
     kwargs.setdefault('ROUGE_SU4', False)
     kwargs.setdefault('stemming', False)
+    kwargs.setdefault('favor', True)  # use alpha
     return Pythonrouge(**kwargs)
+
+
+def rouge_n(summary, references, n, alpha):
+    """
+    Calculate ROUGE-N using pythonrouge.
+    Args have the same meaning as our rouge.rouge_n.
+    :param summary:
+    :param references:
+    :param n:
+    :param alpha:
+    :return:
+    """
+    rouge = make_rouge(summary=[[summary]], reference=[[references]], n_gram=n, p=alpha)
+    score = rouge.calc_score()
+    prefix = 'ROUGE-%d-' % n
+    return [score[prefix + kind] for kind in ['R', 'P', 'F']]
--- tests/test_rouge_n.py
+++ tests/test_rouge_n.py
@@ -1,16 +1,23 @@
 import unittest
 from rouge.rouge import rouge_n
 from rouge.tests import make_rouge
+from rouge.tests import rouge_n as _rouge_n
 
 
 class TestRougeN(unittest.TestCase):
-    summary = 'gunman kill the police'
-    references = 'the police killed the gunman'
+    summary = 'gunman kill the police'.split()
+    references = 'the police killed the gunman'.split()
 
     def test_rouge_2(self):
-        r, p, f = rouge_n(self.summary.split(), [self.references.split()])
-        rouge = make_rouge(summary=[[self.summary]], reference=[[[self.references]]], n_gram=2)
-        score = rouge.calc_score()
-        self.assertAlmostEqual(r, score['ROUGE-2-R'], places=5)
-        self.assertAlmostEqual(p, score['ROUGE-2-P'], places=5)
-        self.assertAlmostEqual(f, score['ROUGE-2-F'], places=5)
+        r, p, f = rouge_n(self.summary, [self.references])
+        r_, p_, f_ = _rouge_n(self.summary, [self.references], n=2, alpha=0.5)
+        self.assertAlmostEqual(r, r_, places=5)
+        self.assertAlmostEqual(p, p_, places=5)
+        self.assertAlmostEqual(f, f_, places=5)
+
+    def test_rouge_1(self):
+        r, p, f = rouge_n(self.summary, [self.references], n=1)
+        r_, p_, f_ = _rouge_n(self.summary, [self.references], n=1, alpha=0.5)
+        self.assertAlmostEqual(r, r_, places=5)
+        self.assertAlmostEqual(p, p_, places=5)
+        self.assertAlmostEqual(f, f_, places=5)
